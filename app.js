7018eed0aa2370698c8ca667901204b62f41a046
jorgitoluis <jorgitoluisolea@gmail.com>
2023-08-05 00:52:02 -0300

    formulario terminado

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")))
 
 app.get("/home",(req,res) => res.sendFile(path.join(__dirname,"views","home.html"))) 
 
+app.get("/login",(req,res) => res.sendFile(path.join(__dirname,"views","login.html"))) 
+
+app.get("/register",(req,res) => res.sendFile(path.join(__dirname,"views","register.html"))) 
+
 app.listen(PORT, () => console.log(`servidor corriendo en http://localhost:`+PORT))
+
